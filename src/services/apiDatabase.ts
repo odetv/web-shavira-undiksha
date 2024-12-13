f92926a8d28dea8c7655ae45dfa14803969a6bc0
odetv <abdiutama02@gmail.com>
2024-12-13 21:21:58 +0800

    feat: refactor firebase config and add handle auth with google

--- src/services/apiDatabase.ts
+++ src/services/apiDatabase.ts
@@ -1,27 +1,14 @@
-import { initializeApp } from "firebase/app";
+import { auth, db } from "@/services/firebase";
 import {
-  getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   updateProfile,
+  GoogleAuthProvider,
+  signInWithPopup,
   signOut,
 } from "firebase/auth";
-import { getFirestore, doc, setDoc, Timestamp } from "firebase/firestore";
+import { doc, setDoc, Timestamp, getDoc } from "firebase/firestore";
 import bcrypt from "bcryptjs";
-
-const firebaseConfig = {
-  apiKey: `${process.env.NEXT_PUBLIC_FIREBASE_APIKEY}`,
-  authDomain: `${process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN}`,
-  projectId: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECTID}`,
-  storageBucket: `${process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET}`,
-  messagingSenderId: `${process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID}`,
-  appId: `${process.env.NEXT_PUBLIC_FIREBASE_APPID}`,
-  measurementId: `${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID}`,
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
 
 export const signUpManual = async (
   name: string,
@@ -71,6 +58,70 @@ export const signInManual = async (email: string, password: string) => {
     return userCredential.user;
   } catch (error: any) {
     console.error("Error signing in: ", error);
+    throw new Error(error.message);
+  }
+};
+
+export const signInGoogle = async () => {
+  const provider = new GoogleAuthProvider();
+  try {
+    const result = await signInWithPopup(auth, provider);
+    const user = result.user;
+
+    const userDocRef = doc(db, "users", user.uid);
+    const userDocSnap = await getDoc(userDocRef);
+
+    if (!userDocSnap.exists()) {
+      const userData = {
+        uid: user.uid,
+        name: user.displayName || "Nama Tidak Tersedia",
+        email: user.email,
+        photo_url:
+          user.photoURL ||
+          "https://static.vecteezy.com/system/resources/thumbnails/003/337/584/small/default-avatar-photo-placeholder-profile-icon-vector.jpg",
+        role: "registered",
+        status: "active",
+        type_user: "google",
+        created_at: Timestamp.fromDate(new Date()),
+        updated_at: Timestamp.fromDate(new Date()),
+      };
+      await setDoc(userDocRef, userData);
+    }
+    return user;
+  } catch (error: any) {
+    console.error("Error signing in with Google: ", error);
+    throw new Error(error.message);
+  }
+};
+
+export const signUpGoogle = async () => {
+  const provider = new GoogleAuthProvider();
+  try {
+    const result = await signInWithPopup(auth, provider);
+    const user = result.user;
+
+    const userDocRef = doc(db, "users", user.uid);
+    const userDocSnap = await getDoc(userDocRef);
+
+    if (!userDocSnap.exists()) {
+      const userData = {
+        uid: user.uid,
+        name: user.displayName || "Nama Tidak Tersedia",
+        email: user.email,
+        photo_url:
+          user.photoURL ||
+          "https://static.vecteezy.com/system/resources/thumbnails/003/337/584/small/default-avatar-photo-placeholder-profile-icon-vector.jpg",
+        role: "registered",
+        status: "active",
+        type_user: "google",
+        created_at: Timestamp.fromDate(new Date()),
+        updated_at: Timestamp.fromDate(new Date()),
+      };
+      await setDoc(userDocRef, userData);
+    }
+    return user;
+  } catch (error: any) {
+    console.error("Error signing up with Google: ", error);
     throw new Error(error.message);
   }
 };
