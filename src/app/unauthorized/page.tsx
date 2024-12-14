065fc2adaaacbb3d939508eccd970874a8a6226e
odetv <abdiutama02@gmail.com>
2024-12-14 21:52:36 +0800

    feat: adding new page for handle 403 (but still not in use)

---
+++ src/app/unauthorized/page.tsx
@@ -0,0 +1,8 @@
+export default function Unauthorized() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Akses Ditolak</h1>
+      <p>Maaf, Anda tidak memiliki akses untuk melihat halaman ini.</p>
+    </div>
+  );
+}
