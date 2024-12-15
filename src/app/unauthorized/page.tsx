4a08c4083051fced86d9cf56d25b6582c875c3f5
odetv <abdiutama02@gmail.com>
2024-12-15 08:28:39 +0800

    fix: change the option for limit access admin panels using traditional step (not recommended)

--- src/app/unauthorized/page.tsx
+++ src/app/unauthorized/page.tsx
@@ -1,8 +1,20 @@
-export default function Unauthorized() {
+import Link from "next/link";
+
+export default function AccessNotAllowed() {
   return (
-    <div style={{ textAlign: "center", padding: "50px" }}>
-      <h1>Akses Ditolak</h1>
-      <p>Maaf, Anda tidak memiliki akses untuk melihat halaman ini.</p>
-    </div>
+    <main className="min-h-screen flex justify-center items-center -mt-20">
+      <div
+        id="info-bot"
+        className="text-center text-black tracking-wide flex flex-col items-center justify-center"
+      >
+        <h1 className="text-3xl sm:text-5xl font-bold pb-2">403 Forbidden</h1>
+        <Link
+          href="/"
+          className="text-sm bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 cursor-pointer transition-all ease-in-out"
+        >
+          Kembali ke Beranda
+        </Link>
+      </div>
+    </main>
   );
 }
